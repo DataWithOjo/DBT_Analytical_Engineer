258d72291193c132549e5f9959277953ba3a49ae
Oluwakayode Ojo <ojokayode80013@gmail.com>
2026-01-02 18:23:23 +0000

    Setup the source yml file for the jaffle_shop and stripe schema. Referenced the source in the staging models

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -3,5 +3,6 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    amount
-from raw.stripe.payment
+    amount / 100 as amount,
+    created as created_at
+from {{ source('stripe', 'payments') }}
